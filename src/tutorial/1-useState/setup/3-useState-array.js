b14083141238f81227b54fa9728375d938411320
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-14 20:04:26 +0200

    practiced using usestate on an array and removing data by id & all

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,27 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const removePerson = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople);
+  }
+
+  return (
+    <div>
+      { people.map((person)=>{
+        const {id, name} = person
+        return (
+          <div key={id} className="item" >
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removePerson(id)}>Remove</button>
+          </div>
+        )
+        })}
+        <button className="btn" onClick={() => setPeople([])}>Delete</button>
+    </div>
+  )
 };
 
 export default UseStateArray;

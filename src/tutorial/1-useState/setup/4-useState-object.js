fccad37a1bfca0056e9c1814a43d4e856655aded
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-14 20:22:05 +0200

    practiced using useState with objects and destructing them and using spread operator to save values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'kate', 
+    age: 30, 
+    message: 'hello you'
+  });
+
+  const changeMessage = () => {
+    setPerson({...person, message: "new message", name: "james"});
+  }
+  return (
+    <di>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Update
+      </button>
+    </di>);
 };
 
 export default UseStateObject;

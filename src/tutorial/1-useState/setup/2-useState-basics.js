dbc2971286f59cbe9018f9e949992ee1ec1d158f
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-14 19:01:39 +0200

    practiced using useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('Hi there');
+  const handleClick = () => {
+    if (text === 'Hi there') {
+      setText('Kathy');
+    } else {
+      setText('Hi there');
+    }
+    
+  }
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button type='button' className="btn" onClick={handleClick}>
+        CHange title
+      </button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;

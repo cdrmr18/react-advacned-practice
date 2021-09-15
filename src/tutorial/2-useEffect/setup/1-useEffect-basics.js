95829c97b5a45e526b834891223c9db0913b5fb0
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-15 07:53:32 +0200

    practiced useEfect basics with conditionals

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,21 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(()=> {
+    console.log('effect');
+    if (value > 1) {
+      document.title = `New Message(${value})`;
+    }
+  }, [value]);
+  
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button className="btn" onClick={()=>setValue(value + 1)}>Submit</button>
+    </div>
+  )
 };
 
 export default UseEffectBasics;

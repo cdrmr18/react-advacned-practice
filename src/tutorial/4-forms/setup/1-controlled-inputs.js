d15963bfb7559df8db56d08458603ce2e3f8f0df
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-18 22:27:57 +0200

    finished practicing controlled inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -13,14 +13,12 @@ const ControlledInputs = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (firstName && email) {
-      const person = {firstName, email};
+      const person = {id: new Date().getTime().toString(),firstName, email};
       setPeople((people)=>{
         return [...people, person]
       });
       setFirstName('');
       setEmail('');
-    } else {
-
     }
   }
   return (
@@ -37,6 +35,13 @@ const ControlledInputs = () => {
           </div>
           <button type="submit">add person</button>
         </form>
+        {people.map((person)=>{
+          const {id, firstName, email} = person;
+          return <div className="item" key={id} >
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        })}
       </article>
     </div>
   )
